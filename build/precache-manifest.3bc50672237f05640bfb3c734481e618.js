27cf0a0a4df1f985987b30780df97edd3683d33c
Jordan Kaiser <jordan.kaiser@gmail.com>
2020-05-06 12:07:40 -0400

    build and deploy

---
+++ build/precache-manifest.3bc50672237f05640bfb3c734481e618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b6c695249cf1a53a57064586bcaa8d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c27e62d95fe1f72be2e2",
+    "url": "/clicky-game/static/css/main.dd15e40b.chunk.css"
+  },
+  {
+    "revision": "80b137b6cf82fa8bc9bf",
+    "url": "/clicky-game/static/js/2.c897a142.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.c897a142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27e62d95fe1f72be2e2",
+    "url": "/clicky-game/static/js/main.58d213f8.chunk.js"
+  },
+  {
+    "revision": "b631d7080dcb249ab8e5",
+    "url": "/clicky-game/static/js/runtime-main.ea51dac7.js"
+  }
+]);
