f58a79f1769047e079401db4da83e54c7befb2b8
Jordan Kaiser <Jordan.kaiser@gmail.com>
2020-05-06 20:22:28 -0400

    Updates

---
+++ precache-manifest.6c2a64fc0b70e5749c2006b8979fb63e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aed493d04e93efb7d92b12a6e9120db",
+    "url": "/git@github.com:Elwdipath/clicky-game.git/index.html"
+  },
+  {
+    "revision": "bb70a9113adfecd1ac62",
+    "url": "/git@github.com:Elwdipath/clicky-game.git/static/css/main.dd15e40b.chunk.css"
+  },
+  {
+    "revision": "80b137b6cf82fa8bc9bf",
+    "url": "/git@github.com:Elwdipath/clicky-game.git/static/js/2.c897a142.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:Elwdipath/clicky-game.git/static/js/2.c897a142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb70a9113adfecd1ac62",
+    "url": "/git@github.com:Elwdipath/clicky-game.git/static/js/main.24bc0e53.chunk.js"
+  },
+  {
+    "revision": "87f8df5c7c9b26bd2d32",
+    "url": "/git@github.com:Elwdipath/clicky-game.git/static/js/runtime-main.0110fa8f.js"
+  }
+]);
